3007601ef3e5ea649e39347d54b3b73bcebef0da
Daniel Krueger <keenbug@gmail.com>
2014-03-19 16:20:39 +0100

    introduced distinction between Behaviors and Events

--- Graphics/FRP.hs
+++ Graphics/FRP.hs
@@ -6,10 +6,7 @@ import Control.Monad
 
 data Event a = Event a | NoEvent deriving Show
 
-instance Monad Event where
-  return = Event
-  (Event e) >>= f = f e
-  NoEvent   >>= f = NoEvent
+newtype Behavior a = Behavior { behaviorValue :: a } deriving Show
 
 data FRP a b where
   Constant :: b -> FRP a b
@@ -30,8 +27,8 @@ instance Show (FRP a b) where
 run :: FRP a b -> a -> (b, FRP a b)
 run c@(Constant k)      _ = (k, c)
 run p@(Pure f)          x = (f x, p)
-run (Stateful f state)  x = (newstate, Stateful f newstate)
-  where newstate = f x state
+run (Stateful f value)  x = (newvalue, Stateful f newvalue)
+  where newvalue = f x value
 run (Connect frp0 frp1) x = (res1, Connect nfrp0 nfrp1)
   where
     (res0, nfrp0) = run frp0 x
@@ -55,57 +52,79 @@ getState (Connect _ frp)     = getState frp
 getState (Merge f frp0 frp1) = f (getState frp0) (getState frp1)
 getState (OnEvent frp)       = getState frp
 
-runList network xs = tail $ map fst $ scanl (run . snd) (undefined, network) xs
 
-constant = Constant
-mapB = Pure
+-------- BEHAVIOR -------
 
-input = mapB id
+constant :: a -> FRP x (Behavior a)
+constant = Constant . Behavior
 
-foldp :: (a -> s -> s) -> s -> FRP (Event a) s
-foldp f state = OnEvent $ Stateful f state
+mapBehavior :: (a -> b) -> FRP (Behavior a) (Behavior b)
+mapBehavior f = Pure (Behavior . f . behaviorValue)
 
-sampleOn :: FRP a (Event i) -> FRP a v -> FRP a (Event v)
-sampleOn = merge sample
- where
-   sample (Event _) x = Event x
-   sample NoEvent   _ = NoEvent
 
-maskBy :: FRP a Bool -> FRP a (Event b) -> FRP a (Event b)
-maskBy = merge maskEvent
- where
-   maskEvent True  _ = NoEvent
-   maskEvent False x = x
+-------- EVENTS -------
 
-eventMap :: (a -> b) -> FRP (Event a) (Event b)
-eventMap f = OnEvent $ mapB (Event . f)
+mapEvent :: (a -> b) -> FRP (Event a) (Event b)
+mapEvent f = OnEvent $ Pure (Event . f)
 
-eventsWhen :: FRP g Bool -> FRP g (Event a) -> FRP g (Event a)
-eventsWhen = merge filterEvent
- where
-   filterEvent True ev = ev
-   filterEvent False _ = NoEvent
 
-eventFilter :: (a -> Bool) -> FRP (Event a) (Event a)
-eventFilter pred = OnEvent $ mapB filterEvent
+filterEvent :: (a -> Bool) -> FRP (Event a) (Event a)
+filterEvent pred = OnEvent $ Pure filterEvent
  where
    filterEvent ev = if pred ev then Event ev else NoEvent
  
 eventExtract :: (a -> Maybe b) -> FRP (Event a) (Event b)
-eventExtract extract = OnEvent $ mapB extractEvent
- where
-   extractEvent ev = case extract ev of
-     Just res -> Event res
-     Nothing  -> NoEvent
+eventExtract extract = OnEvent $ Pure (maybeToEvent . extract)
 
-eventMerge :: FRP g (Event a) -> FRP g (Event a) -> FRP g (Event a)
-eventMerge = merge evMerge
+eventMergeLeft :: FRP g (Event a) -> FRP g (Event a) -> FRP g (Event a)
+eventMergeLeft = Merge evMerge
   where
     evMerge x y = case x of
       Event _ -> x
       NoEvent -> y
 
-hold = foldp const
+eventMergeRight :: FRP g (Event a) -> FRP g (Event a) -> FRP g (Event a)
+eventMergeRight = Merge evMerge
+  where
+    evMerge x y = case y of
+      Event _ -> y
+      NoEvent -> x
+
+eventMergeWith :: (Maybe a -> Maybe a -> Maybe a) -> FRP g (Event a) -> FRP g (Event a) -> FRP g (Event a)
+eventMergeWith f evfrp1 evfrp2 = Merge evMerge evfrp1 evfrp2
+  where
+    evMerge ev1 ev2 = maybeToEvent $ f (eventToMaybe ev1) (eventToMaybe ev2)
+
+maybeToEvent (Just x) = Event x
+maybeToEvent Nothing  = NoEvent
+
+eventToMaybe (Event x) = Just x
+eventToMaybe NoEvent   = Nothing
+
+
+
+--------- CONVERT ----------
+
+foldp :: (a -> v -> v) -> v -> FRP (Event a) (Behavior v)
+foldp f init = OnEvent $ Stateful fOnBehavior (Behavior init)
+ where fOnBehavior event (Behavior value) = Behavior (f event value)
+
+sampleOn :: FRP a (Behavior v) -> FRP a (Event i) -> FRP a (Event v)
+behavior `sampleOn` event = Merge sample event behavior
+ where
+   sample (Event _) (Behavior x) = Event x
+   sample NoEvent   _            = NoEvent
+
+
+--------- COMBINATORS --------
+
+filterWhen :: FRP a (Event b) -> FRP a (Behavior Bool) -> FRP a (Event b)
+event `filterWhen` behavior = Merge whenTrue behavior event
+ where
+   whenTrue (Behavior True)  x = x
+   whenTrue (Behavior False) _ = NoEvent
+
+
 
 connect :: FRP a i -> FRP i b -> FRP a b
 connect (Connect a b)        c = connect a (connect b c)
@@ -114,10 +133,19 @@ connect (Pure f) (Connect (Pure g) c) = Connect (Pure (g . f)) c
 connect (OnEvent f) g = OnEvent (connect f g)
 connect a b = Connect a b
 
-merge :: (i -> j -> b) -> FRP a i -> FRP a j -> FRP a b
-merge mrg (Pure f) (Pure g) = Pure $ \x -> mrg (f x) (g x)
+
+merge :: (i -> j -> b) -> FRP a (Behavior i) -> FRP a (Behavior j) -> FRP a (Behavior b)
+merge mrg (Pure f)    (Pure g)    = Pure $ \x -> Behavior $ mrg (behaviorValue $ f x) (behaviorValue $ g x)
 merge mrg (OnEvent f) (OnEvent g) = OnEvent $ merge mrg f g
-merge mrg a        b        = Merge mrg a b
+merge mrg a           b           = nativeMerge mrg a b
+
+nativeMerge :: (i -> j -> b) -> FRP a (Behavior i) -> FRP a (Behavior j) -> FRP a (Behavior b)
+nativeMerge mrg a b = Merge mrgBehaviors a b
+  where mrgBehaviors (Behavior i) (Behavior j) = Behavior (mrg i j)
+
+
+
+--------- INFIX STUFF ---------
 
 infixr 1 >>>
 (>>>) = connect
@@ -125,8 +153,8 @@ infixl 1 <<<
 (<<<) = flip (>>>)
 
 infixl 1 <~
-(<~) :: (a -> b) -> FRP i a -> FRP i b
-f <~ x = mapB f <<< x
+(<~) :: (a -> b) -> FRP i (Behavior a) -> FRP i (Behavior b)
+f <~ x = mapBehavior f <<< x
 infixl 1 ~~
-(~~) :: FRP i (a -> b) -> FRP i a -> FRP i b
+(~~) :: FRP i (Behavior (a -> b)) -> FRP i (Behavior a) -> FRP i (Behavior b)
 f ~~ x = merge ($) f x
--- Graphics/FRPUtils.hs
+++ Graphics/FRPUtils.hs
@@ -2,8 +2,14 @@ module Graphics.FRPUtils where
 
 import Graphics.FRP
 
-lastE :: Int -> a -> FRP (Event a) [a]
-lastE n init = foldp newEvent inits
+runList network xs = tail $ map fst $ scanl (run . snd) (undefined, network) xs
+
+lastEvents :: Int -> a -> FRP (Event a) (Behavior [a])
+lastEvents n init = foldp newEvent inits
   where
     inits = replicate n init
     newEvent new before = take n $ new : before
+
+hold :: a -> FRP (Event a) (Behavior a)
+hold init = foldp takeNewValue init
+  where takeNewValue newValue oldValue = newValue
