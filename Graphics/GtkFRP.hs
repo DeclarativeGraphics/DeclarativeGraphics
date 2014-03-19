8a87b07ec177ae5a98abbfae4de9429602c85ee2
Daniel Krueger <keenbug@gmail.com>
2014-03-19 22:10:58 +0100

    fixed GtkFRP to implement WidgetFRP

--- Graphics/GtkFRP.hs
+++ Graphics/GtkFRP.hs
@@ -5,136 +5,77 @@ import Data.IORef
 
 import Graphics.FRP
 import Graphics.FRPUtils
+import Graphics.WidgetFRP
 
 import Graphics.UI.Gtk hiding (Point)
 import qualified Graphics.Rendering.Cairo as CG
 import Graphics.Rendering.Cairo (liftIO)
 
 
-data GtkEvent = MouseMove (Point Double)
-              | MousePress MouseButton
-              | MouseRelease MouseButton
-              | KeyPress KeyVal
-              | KeyRelease KeyVal
-              | Resize (Int,Int)
-              deriving (Eq,Show)
-
-type GtkFRP state = FRP (Event GtkEvent) state
-
-type Point a = (a,a)
-type RGB a = (a,a,a)
-
-
 frpWidget :: (DrawingArea -> state -> IO ()) -- ^ rendering function
           -> DrawingArea -- ^ the drawable
-          -> GtkFRP state -- ^ the FRP system
+          -> WidgetFRP state -- ^ the FRP system
           -> IO ()
 frpWidget renderFRPState canvas frpsys = do
       frp <- newIORef (getState frpsys, frpsys)
 
-      let draw = renderFRPState canvas
-
-      canvas `on` exposeEvent $ processEvent $ liftIO $ do
+      canvas `on` exposeEvent $ processGtkEvent $ liftIO $ do
         draw =<< getFRPState frp
 
-      canvas `on` motionNotifyEvent $ processEvent $ do
+      canvas `on` motionNotifyEvent $ processGtkEvent $ do
         coord <- eventCoordinates
-        liftIO $ draw =<< event frp (MouseMove coord)
+        liftIO $ processFRPEvent frp MouseMove coord
         eventRequestMotions
       
-      canvas `on` buttonPressEvent $ processEvent $ do
+      canvas `on` buttonPressEvent $ processGtkEvent $ do
         button <- eventButton
-        liftIO $ draw =<< event frp (MousePress button)
+        liftIO $ maybe (return ()) (processFRPEvent frp MousePress) (convertMouseButton button)
 
-      canvas `on` buttonReleaseEvent $ processEvent $ do
+      canvas `on` buttonReleaseEvent $ processGtkEvent $ do
         button <- eventButton
-        liftIO $ draw =<< event frp (MouseRelease button)
+        liftIO $ maybe (return ()) (processFRPEvent frp MouseRelease) (convertMouseButton button)
 
-      canvas `on` keyPressEvent $ processEvent $ do
+      canvas `on` keyPressEvent $ processGtkEvent $ do
         key <- eventKeyVal
-        liftIO $ draw =<< event frp (KeyPress key)
+        liftIO $ maybe (return ()) (processFRPEvent frp KeyPress) (convertKey key)
 
-      canvas `on` keyReleaseEvent $ processEvent $ do
+      canvas `on` keyReleaseEvent $ processGtkEvent $ do
         key <- eventKeyVal
-        liftIO $ draw =<< event frp (KeyRelease key)
+        liftIO $ maybe (return ()) (processFRPEvent frp KeyRelease) (convertKey key)
 
-      canvas `on` configureEvent $ processEvent $ do
+      canvas `on` configureEvent $ processGtkEvent $ do
         size <- eventSize
-        liftIO $ draw =<< event frp (Resize size)
+        liftIO $ processFRPEvent frp Resize size
 
       return ()
   where
-    processEvent :: Monad m => m a -> m Bool
-    processEvent = (>> return True)
+    draw = renderFRPState canvas . behaviorValue
+
+    processGtkEvent :: Monad m => m a -> m Bool
+    processGtkEvent = (>> return True)
+
+    processFRPEvent frp eventType eventValue
+      = draw =<< event frp (eventType eventValue)
+
     event frpref ev = do
       (_,actfrp) <- readIORef frpref
       let newfrpsys = run actfrp (Event ev)
       writeIORef frpref newfrpsys
       return (frpstate newfrpsys)
+
     frpstate (state,_) = state
     getFRPState = liftM frpstate . readIORef
 
 
---------- FRP ---------
-
-resizeEvent :: FRP (Event GtkEvent) (Event (Int,Int))
-resizeEvent = eventExtract getResizeSize
-  where
-    getResizeSize (Resize size) = Just size
-    getResizeSize _ = Nothing
-
-size :: FRP (Event (Int,Int)) (Int,Int)
-size = hold (0,0)
+convertMouseButton button = case button of
+  LeftButton -> Just MBLeft
+  RightButton -> Just MBRight
+  MiddleButton -> Just MBMiddle
+  _ -> Nothing
 
 
-
----- MOUSE ----
-
-mouseMove :: FRP (Event GtkEvent) (Event (Point Double))
-mouseMove = eventExtract getMouseMovePosition
-  where
-    getMouseMovePosition (MouseMove pos) = Just pos
-    getMouseMovePosition _ = Nothing
-
-mousePress :: FRP (Event GtkEvent) (Event MouseButton)
-mousePress = eventExtract getMousePressButton
-  where
-    getMousePressButton (MousePress btn) = Just btn
-    getMousePressButton _ = Nothing
-
-mouseRelease :: FRP (Event GtkEvent) (Event MouseButton)
-mouseRelease = eventExtract getMouseReleaseButton
-  where
-    getMouseReleaseButton (MouseRelease btn) = Just btn
-    getMouseReleaseButton _ = Nothing
-
-mousePos :: FRP (Event GtkEvent) (Point Double)
-mousePos = hold (0,0) <<< mouseMove
-
-
-
----- KEY ----
-
-keyPress :: FRP (Event GtkEvent) (Event Key)
-keyPress = eventExtract getKeyPressKey
-  where
-    getKeyPressKey (KeyPress keyval) = Just (keyValToKey keyval)
-    getKeyPressKey _ = Nothing
-
-keyRelease :: FRP (Event GtkEvent) (Event KeyVal)
-keyRelease = eventExtract getKeyReleaseKey
-  where
-    getKeyReleaseKey (KeyRelease key) = Just key
-    getKeyReleaseKey _ = Nothing
-
-
-
-data Key = Return
-         | Spacebar
-         | Letter Char
-         deriving (Show)
-
-keyValToKey key = case keyName key of
-  "Return" -> Return
-  "space"  -> Spacebar
-  [c]      -> Letter c
+convertKey key = case keyName key of
+  "Return" -> Just Return
+  "space"  -> Just Space
+  [c]      -> Just (Letter c)
+  _        -> Nothing
