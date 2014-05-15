b3e06219c39bccfc91cfe512c5983cdabe3f0516
Jonas Haag <jonas@lophus.org>
2014-05-15 22:09:22 +0200

    Import Automaton.elm

--- Automaton.hs
+++ Automaton.hs
@@ -1,5 +1,7 @@
 module Automaton where
 
+import Utils
+
 ----- FRP BASE -----
 
 data Automaton a b = Step (a -> (Automaton a b, b))
@@ -7,127 +9,121 @@ data Automaton a b = Step (a -> (Automaton a b, b))
 data Event a = Event a | NoEvent
 data Behavior a = Behavior a
 
-step : Automaton a b -> a -> (Automaton a b, b)
+step :: Automaton a b -> a -> (Automaton a b, b)
 step (Step f) input = f input
 
 
 ----- HELPERS -----
 
-onEvent : (a -> b) -> Event a -> Event b
+onEvent :: (a -> b) -> Event a -> Event b
 onEvent f ev = case ev of
   Event x -> Event (f x)
   NoEvent -> NoEvent
 
-extractValue : Behavior a -> a
+extractValue :: Behavior a -> a
 extractValue (Behavior x) = x
 
-onBehavior : (a -> b) -> Behavior a -> Behavior b
+onBehavior :: (a -> b) -> Behavior a -> Behavior b
 onBehavior f (Behavior x) = Behavior (f x)
 
-onBehavior2 : (a -> b -> c) -> Behavior a -> Behavior b -> Behavior c
+onBehavior2 :: (a -> b -> c) -> Behavior a -> Behavior b -> Behavior c
 onBehavior2 f (Behavior x) (Behavior y) = Behavior (f x y)
 
 
 ----- COMBINATORS ETC. -----
 
-mapEvent : (a -> b) -> Automaton (Event a) (Event b)
+mapEvent :: (a -> b) -> Automaton (Event a) (Event b)
 mapEvent f = Step <| \ input -> (mapEvent f, onEvent f input)
 
-mergeEvents : Event a -> Event a -> Event a
+mergeEvents :: Event a -> Event a -> Event a
 mergeEvents ev1 ev2 = case ev1 of
   Event _ -> ev1
   _       -> ev2
 
-mapBehavior : (a -> b) -> Automaton (Behavior a) (Behavior b)
+mapBehavior :: (a -> b) -> Automaton (Behavior a) (Behavior b)
 mapBehavior f = Step <| \ input -> (mapBehavior f, onBehavior f input)
 
-constBehavior : a -> Automaton i (Behavior a)
+constBehavior :: a -> Automaton i (Behavior a)
 constBehavior x = Step <| \ _ -> (constBehavior x, Behavior x)
 
 
 
-andThen : Automaton a b -> Automaton b c -> Automaton a c
+andThen :: Automaton a b -> Automaton b c -> Automaton a c
 andThen a b = Step (\ input -> let (newA, outA) = step a input
                                    (newB, outB) = step b outA
                                in (andThen newA newB, outB))
 
-(>>>) : Automaton a b -> Automaton b c -> Automaton a c
+(>>>) :: Automaton a b -> Automaton b c -> Automaton a c
 (>>>) = andThen
-(<<<) : Automaton b c -> Automaton a b -> Automaton a c
+(<<<) :: Automaton b c -> Automaton a b -> Automaton a c
 (<<<) = flip (>>>)
 
-swap : Automaton (a,b) (b,a)
+swap :: Automaton (a,b) (b,a)
 swap = liftA (\ (x,y) -> (y,x))
 
                                 
-keepWhen : Behavior Bool -> Event a -> Event a
+keepWhen :: Behavior Bool -> Event a -> Event a
 keepWhen pred ev = if extractValue pred then ev else NoEvent
 
-dropWhen : Behavior Bool -> Event a -> Event a
+dropWhen :: Behavior Bool -> Event a -> Event a
 dropWhen = keepWhen . onBehavior not
 
-identity : Automaton a a
+identity :: Automaton a a
 identity = liftA id
 
-foldEvents : (a -> b -> b) -> b -> Automaton (Event a) (Behavior b)
+foldEvents :: (a -> b -> b) -> b -> Automaton (Event a) (Behavior b)
 foldEvents f init = Step <| \ input -> case input of
   NoEvent -> (foldEvents f init, Behavior init)
   Event ev -> let newState = f ev init
               in (foldEvents f newState, Behavior newState)
 
-accumEvents : a -> Automaton (Event (a -> a)) (Behavior a)
+accumEvents :: a -> Automaton (Event (a -> a)) (Behavior a)
 accumEvents init = Step <| \ input -> case input of
   NoEvent -> (accumEvents init, Behavior init)
   Event f -> let newState = f init
              in (accumEvents newState, Behavior newState)
 
-splitEvents : Automaton (Event (Either a b)) (Event a, Event b)
-splitEvents = Step <| \ input -> case input of
-                                   NoEvent -> (NoEvent, NoEvent)
-                                   Event (Left l) -> (Event l, NoEvent)
-                                   Event (Right r) -> (NoEvent, Event r)
+splitEvents :: Automaton (Event (Either a b)) (Event a, Event b)
+splitEvents = Step <| \ input -> (splitEvents,
+                                  case input of
+                                    NoEvent -> (NoEvent, NoEvent)
+                                    Event (Left l) -> (Event l, NoEvent)
+                                    Event (Right r) -> (NoEvent, Event r))
 
 
-filterEvents : (a -> Bool) -> Automaton (Event a) (Event a)
+filterEvents :: (a -> Bool) -> Automaton (Event a) (Event a)
 filterEvents pred = Step (\ input -> (filterEvents pred,
                                       case input of
                                         Event x -> if pred x then Event x else NoEvent
                                         NoEvent -> NoEvent))
 
 
-filterJust : Automaton (Event (Maybe a)) (Event a)
+filterJust :: Automaton (Event (Maybe a)) (Event a)
 filterJust = Step (\ input -> case input of
                                 NoEvent -> (filterJust, NoEvent)
                                 Event maybeX -> (filterJust, maybe NoEvent Event maybeX))
 
-holdLast : a -> Automaton (Event a) (Behavior a)
+holdLast :: a -> Automaton (Event a) (Behavior a)
 holdLast init = foldEvents (\n _ -> n) init
 
-splitMerge : (a -> b -> c) -> Automaton g a -> Automaton g b -> Automaton g c
+splitMerge :: (a -> b -> c) -> Automaton g a -> Automaton g b -> Automaton g c
 splitMerge f a b = Step (\ input -> let (newA, outA) = step a input
                                         (newB, outB) = step b input
                                     in (splitMerge f newA newB, f outA outB))
 
-parallel : Automaton ai ao -> Automaton bi bo -> Automaton (ai,bi) (ao,bo)
+parallel :: Automaton ai ao -> Automaton bi bo -> Automaton (ai,bi) (ao,bo)
 parallel a b = fork (a <<< liftA fst) (b <<< liftA snd)
 
-fork : Automaton g a -> Automaton g b -> Automaton g (a,b)
+fork :: Automaton g a -> Automaton g b -> Automaton g (a,b)
 fork a b = splitMerge (,) a b
 
-merge : (a -> b -> c) -> Automaton (a,b) c
+merge :: (a -> b -> c) -> Automaton (a,b) c
 merge f = liftA (uncurry f)
 
-liftA : (a -> b) -> Automaton a b
+liftA :: (a -> b) -> Automaton a b
 liftA f = Step (\ input -> (liftA f, f input)) 
 
 
-loop : state -> Automaton (i,state) (o,state) -> Automaton i o
+loop :: state -> Automaton (i,state) (o,state) -> Automaton i o
 loop init aut = Step (\ input -> let (newAut,(outAut,stateAut)) = step aut (input,init)
                                  in (loop stateAut newAut, outAut))
-
------ ELM INTERFACE -----
-run : Automaton (Event i) (Behavior o) -> Signal i -> Signal o
-run automaton sig =
-  let (aut,init) = step automaton NoEvent
-      stepInput input (automaton,state) = step automaton (Event input)
-  in lift (extractValue . snd) <| foldp stepInput (aut,init) sig
