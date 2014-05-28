4ca9b30e12f09a5743f1423d20edda073dd9069a
Jonas Haag <jonas@lophus.org>
2014-05-28 20:09:28 +0200

    Automaton -> FRP

--- FRP.hs
+++ FRP.hs
@@ -1,18 +1,37 @@
-module Automaton where
+module FRP where
 
+import Prelude hiding ((.),id)
+import Control.Category
+import Control.Arrow
 import Utils
+
+instance Category FRP where
+  id = identity
+  (.) = flip andThen
+
+instance Arrow FRP where
+  arr = liftFRP
+  first frp = parallel frp identity
+  second frp = parallel identity frp
+  (***) = parallel
+  (&&&) = fork
+
+instance ArrowLoop FRP where
+  loop aut = Step (\ input -> let (newAut,(outAut,state)) = step aut (input,state)
+                              in (loop newAut, outAut))
+
 
 ----- FRP BASE -----
 
-data Automaton a b = Step (a -> (Automaton a b, b))
+data FRP a b = Step (a -> (FRP a b, b))
 
-data Event a = Event a | NoEvent
-data Behavior a = Behavior a
+data Event a = Event a | NoEvent deriving (Show)
+data Behavior a = Behavior a deriving (Show)
 
-step :: Automaton a b -> a -> (Automaton a b, b)
+step :: FRP a b -> a -> (FRP a b, b)
 step (Step f) input = f input
 
-stepEvent :: Automaton (Event a) b -> a -> (Automaton (Event a) b, b)
+stepEvent :: FRP (Event a) b -> a -> (FRP (Event a) b, b)
 stepEvent automaton x = step automaton (Event x)
 
 
@@ -35,34 +54,32 @@ onBehavior2 f (Behavior x) (Behavior y) = Behavior (f x y)
 
 ----- COMBINATORS ETC. -----
 
-mapEvent :: (a -> b) -> Automaton (Event a) (Event b)
-mapEvent f = Step <| \ input -> (mapEvent f, onEvent f input)
+liftFRP :: (a -> b) -> FRP a b
+liftFRP f = Step (\ input -> (liftFRP f, f input)) 
+
+mapEvents :: (a -> b) -> FRP (Event a) (Event b)
+mapEvents f = Step <| \ input -> (mapEvents f, onEvent f input)
 
 mergeEvents :: Event a -> Event a -> Event a
 mergeEvents ev1 ev2 = case ev1 of
   Event _ -> ev1
   _       -> ev2
 
-mapBehavior :: (a -> b) -> Automaton (Behavior a) (Behavior b)
+mapBehavior :: (a -> b) -> FRP (Behavior a) (Behavior b)
 mapBehavior f = Step <| \ input -> (mapBehavior f, onBehavior f input)
 
-constBehavior :: a -> Automaton i (Behavior a)
+constBehavior :: a -> FRP i (Behavior a)
 constBehavior x = Step <| \ _ -> (constBehavior x, Behavior x)
 
 
 
-andThen :: Automaton a b -> Automaton b c -> Automaton a c
+andThen :: FRP a b -> FRP b c -> FRP a c
 andThen a b = Step (\ input -> let (newA, outA) = step a input
                                    (newB, outB) = step b outA
                                in (andThen newA newB, outB))
 
-(>>>) :: Automaton a b -> Automaton b c -> Automaton a c
-(>>>) = andThen
-(<<<) :: Automaton b c -> Automaton a b -> Automaton a c
-(<<<) = flip (>>>)
-
-swap :: Automaton (a,b) (b,a)
-swap = liftA (\ (x,y) -> (y,x))
+swap :: FRP (a,b) (b,a)
+swap = liftFRP (\ (x,y) -> (y,x))
 
                                 
 keepWhen :: Behavior Bool -> Event a -> Event a
@@ -71,22 +88,22 @@ keepWhen pred ev = if behaviorValue pred then ev else NoEvent
 dropWhen :: Behavior Bool -> Event a -> Event a
 dropWhen = keepWhen . onBehavior not
 
-identity :: Automaton a a
-identity = liftA id
+identity :: FRP a a
+identity = liftFRP id
 
-foldEvents :: (b -> a -> b) -> b -> Automaton (Event a) (Behavior b)
+foldEvents :: (b -> a -> b) -> b -> FRP (Event a) (Behavior b)
 foldEvents f init = Step <| \ input -> case input of
   NoEvent -> (foldEvents f init, Behavior init)
   Event ev -> let newState = f init ev
               in (foldEvents f newState, Behavior newState)
 
-accumEvents :: a -> Automaton (Event (a -> a)) (Behavior a)
+accumEvents :: a -> FRP (Event (a -> a)) (Behavior a)
 accumEvents init = Step <| \ input -> case input of
   NoEvent -> (accumEvents init, Behavior init)
   Event f -> let newState = f init
              in (accumEvents newState, Behavior newState)
 
-splitEvents :: Automaton (Event (Either a b)) (Event a, Event b)
+splitEvents :: FRP (Event (Either a b)) (Event a, Event b)
 splitEvents = Step <| \ input -> (splitEvents,
                                   case input of
                                     NoEvent -> (NoEvent, NoEvent)
@@ -94,39 +111,51 @@ splitEvents = Step <| \ input -> (splitEvents,
                                     Event (Right r) -> (NoEvent, Event r))
 
 
-filterEvents :: (a -> Bool) -> Automaton (Event a) (Event a)
+filterEvents :: (a -> Bool) -> FRP (Event a) (Event a)
 filterEvents pred = Step (\ input -> (filterEvents pred,
                                       case input of
                                         Event x -> if pred x then Event x else NoEvent
                                         NoEvent -> NoEvent))
 
 
-filterJust :: Automaton (Event (Maybe a)) (Event a)
+filterJust :: FRP (Event (Maybe a)) (Event a)
 filterJust = Step (\ input -> case input of
                                 NoEvent -> (filterJust, NoEvent)
                                 Event maybeX -> (filterJust, maybe NoEvent Event maybeX))
 
-holdLast :: a -> Automaton (Event a) (Behavior a)
+holdLast :: a -> FRP (Event a) (Behavior a)
 holdLast init = foldEvents (\_ n -> n) init
 
-splitMerge :: (a -> b -> c) -> Automaton g a -> Automaton g b -> Automaton g c
+splitMerge :: (a -> b -> c) -> FRP g a -> FRP g b -> FRP g c
 splitMerge f a b = Step (\ input -> let (newA, outA) = step a input
                                         (newB, outB) = step b input
                                     in (splitMerge f newA newB, f outA outB))
 
-parallel :: Automaton ai ao -> Automaton bi bo -> Automaton (ai,bi) (ao,bo)
-parallel a b = fork (a <<< liftA fst) (b <<< liftA snd)
+delay :: FRP (Event a) (Event a)
+delay = delay' NoEvent
 
-fork :: Automaton g a -> Automaton g b -> Automaton g (a,b)
+delay' :: Event a -> FRP (Event a) (Event a)
+delay' lastEvent = Step <| \ input -> (delay' input, lastEvent)
+
+parallel :: FRP ai ao -> FRP bi bo -> FRP (ai,bi) (ao,bo)
+parallel a b = fork (a <<< liftFRP fst) (b <<< liftFRP snd)
+
+fork :: FRP g a -> FRP g b -> FRP g (a,b)
 fork a b = splitMerge (,) a b
 
-merge :: (a -> b -> c) -> Automaton (a,b) c
-merge f = liftA (uncurry f)
-
-liftA :: (a -> b) -> Automaton a b
-liftA f = Step (\ input -> (liftA f, f input)) 
+merge :: (a -> b -> c) -> FRP (a,b) c
+merge f = liftFRP (uncurry f)
 
 
-loop :: state -> Automaton (i,state) (o,state) -> Automaton i o
-loop init aut = Step (\ input -> let (newAut,(outAut,stateAut)) = step aut (input,init)
-                                 in (loop stateAut newAut, outAut))
+--- LOOPING ---
+
+loopInit :: state -> FRP (i,state) (o,state) -> FRP i o
+loopInit init frp = Step <| \ input -> let (newFrp,(outFrp,newState)) = step frp (input,init)
+                                       in (loopInit newState newFrp, outFrp)
+
+loopFold :: state -> FRP (i, Behavior state) (o, Event state) -> FRP i o
+loopFold init frp = Step <| \ input -> let (newFrp, (out,newStateEvent)) = step frp (input, Behavior init)
+                                           newState = case newStateEvent of
+                                                        NoEvent -> init
+                                                        Event x -> x
+                                       in (loopFold newState newFrp, out)
