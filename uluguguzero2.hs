9beb83f9c822d78845c9dc210718a1f1625ff762
Daniel Krueger <keenbug@gmail.com>
2014-09-02 22:12:42 +0200

    Prototyp ist fertig. Jetzt geht's ans Crowdfunden

--- uluguguzero2.hs
+++ uluguguzero2.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Data.Maybe
+import Data.List (replicate)
 
 import FRPZero
 import FRPZeroGtk
@@ -31,12 +32,24 @@ renderString = text ourTextStyle
 besides ls = centered $ groupBy toRight ls
 besides2 x y = centered $ groupBy toRight [x, y]
 
-renderTextInput :: TextInput -> Form
-renderTextInput (l,r) = centered <| groupBy toRight [centered leftText, cursor, centered rightText]
+x `above` y = groupBy toBottom [x, y]
+
+renderTextInput :: Bool -> TextInput -> Form
+renderTextInput focused (l,r)
+  = border <| centered <| groupBy toRight [centered leftText, cursor, centered rightText]
   where
     leftText  = text ourTextStyle (reverse l)
     rightText = text ourTextStyle r
-    cursor = rectangle 1 27 |> filled black |> flip withEnvelope emptyEnvelope
+    cursor = if focused
+                then rectangle 2 27 |> filled black |> flip withEnvelope emptyEnvelope
+                else emptyForm
+    border = padded 2 . debugEnv (if focused then orange else grey) . padded 2
+
+
+debugEnv color form
+  = (outlined ((solid color) { lineWidth = 3 }) . fromEnvelope . fEnvelope $ form)
+    `atop` form
+
 
 interpretTextInput :: GtkEvent -> (TextInput -> Maybe TextInput)
 interpretTextInput (KeyPress key) = case key of
@@ -48,85 +61,233 @@ interpretTextInput (KeyPress key) = case key of
 interpretTextInput _ = const Nothing
 
 type StepFunc state = GtkEvent -> state -> Maybe state
-type RenderFunc state = state -> Form
+type RenderFunc state = Bool -> state -> Form
 
-data Widget = forall state . Widget (StepFunc state) state (RenderFunc state)
+data Widget value
+  = forall state .
+      Widget state
+             (StepFunc state)
+             (RenderFunc state)
+             (state -> value)
 
-textInputWidget init = Widget interpretTextInput (toTextInput init) renderTextInput
-
-stepWidget :: GtkEvent -> Widget -> Maybe Widget
-stepWidget event (Widget step state render)
+stepWidget :: GtkEvent -> Widget value -> Maybe (Widget value)
+stepWidget event (Widget state step render valueOf)
   = do newState <- step event state
-       return (Widget step newState render)
+       return (Widget newState step render valueOf)
 
-renderWidget :: Widget -> Form
-renderWidget (Widget step state render) = render state
+valueOfWidget :: Widget value -> value
+valueOfWidget (Widget state step render valueOf) = valueOf state
 
-modeWidget widget
-  = Widget step (False, widget) render
+renderWidget :: Bool -> Widget value -> Form
+renderWidget focused (Widget state step render valueOf) = render focused state
+
+textInputWidget init
+  = Widget (toTextInput init) interpretTextInput renderTextInput fromTextInput
+
+modeWrapper widget
+  = Widget (False, widget) step render (valueOfWidget . snd)
   where
-    step event (False, widget) = progressOuter event (False, widget)
-    step event (True,  widget) = case progressInner event widget of
+    step event (True, widget) = case progressInner event widget of
       Just newWidget -> Just (True, newWidget)
       Nothing        -> progressOuter event (True, widget)
+    step event (False, widget) = progressOuter event (False, widget)
 
     progressInner = stepWidget
 
-    progressOuter (KeyPress (Letter 'i'))       (False, widget)
+    progressOuter (KeyPress (Letter 'i')) (False, widget)
       = Just (True, widget)
-    progressOuter (KeyPress (Special Escape))   (_,      widget)
+    progressOuter (KeyPress (Special Escape)) (True, widget)
       = Just (False, widget)
-    progressOuter _ state = Nothing
+    progressOuter _ _ = Nothing
 
-    debugEnv color form = (outlined (solid color) . fromEnvelope . fEnvelope $ form)
-                          `atop` form
+    render True (False, widget)
+      = debugEnv blue <| renderWidget False widget
+    render True (True, widget)
+      = renderWidget True widget
+    render False (_, widget)
+      = renderWidget False widget
 
-    render (True, widget) = debugEnv green (renderWidget widget)
-    render (False, widget) = debugEnv grey (renderWidget widget)
-      
-modeTextInputWidgetTest
-  = fuckingBigStuff >>^ accum (False, focus initialWidgets)
-                    ^>> renderStuff
+
+modeInputWidget = modeWrapper . textInputWidget
+
+
+{-
+treeWidget parent children
+    progressOuter (KeyPress (Special ArrLeft))  = moveTreeLeft
+    progressOuter (KeyPress (Special ArrRight)) = moveTreeRight
+    progressOuter (KeyPress (Special ArrDown))  = moveTreeDown
+    progressOuter (KeyPress (Special ArrUp))    = moveTreeUp
+    progressOuter (KeyPress (Letter 'a'))       = insertTree (modeInputWidget "")
+    progressOuter (KeyPress (Letter 'x'))       = deleteTree
+    progressOuter _                             = const Nothing
+      -}
+
+data Value = Num Int | Operator Operator deriving (Show,Eq)
+data Operator = Add | Sub | Mul deriving (Show,Eq)
+
+constWidget :: Form -> Form -> value -> Widget value
+constWidget focusedForm unfocusedForm value
+  = Widget () step render (const value)
   where
-    initialWidgets :: [Widget]
-    initialWidgets
-      = modeWidget (textInputWidget "Pihmel")
-      : map textInputWidget ["foo", "bar", "spam", "eggs"]
+    step _ _ = Nothing
+    render True _  = focusedForm
+    render False _ = unfocusedForm
 
-    fuckingBigStuff :: GtkEvent
-                    -> (Bool, Focus Widget)
-                    -> (Bool, Focus Widget)
-    fuckingBigStuff event (False, textInputs) = progressOuter event (False, textInputs)
-    fuckingBigStuff event (True, textInputs) = case progressInner event textInputs of
-      Just newState -> (True, newState)
-      Nothing       -> progressOuter event (True, textInputs)
+operatorExprWidget name operator
+  = constWidget (text ourTextStyle{ textColor = orange } name)
+                (renderString name) (Operator operator)
 
-    progressInner :: GtkEvent
-                  -> Focus Widget
-                  -> Maybe (Focus Widget)
+addOperatorExprWidget = operatorExprWidget "+" Add
+subOperatorExprWidget = operatorExprWidget "-" Sub
+mulOperatorExprWidget = operatorExprWidget "*" Mul
+operatorsSwitchWidget
+  = switchWidget [addOperatorExprWidget, subOperatorExprWidget, mulOperatorExprWidget]
+intValueExprWidget = mapWidgetValue Num . intWidget
+
+mapWidgetValue f (Widget state step render value)
+  = Widget state step render (f . value)
+
+switchWidget :: [Widget value] -> Widget value
+switchWidget widgets
+  = Widget (focus widgets) step render (valueOfWidget . getFocused)
+  where
+    step event focus = case progressInner event focus of
+      Just newFocus -> Just newFocus
+      Nothing       -> progressOuter event focus
+
+    progressInner event focus
+      = do newFocused <- stepWidget event (getFocused focus)
+           return (setFocused newFocused focus)
+
+    progressOuter (KeyPress (Special ArrLeft))  = moveLeft
+    progressOuter (KeyPress (Special ArrRight)) = moveRight
+    progressOuter _                             = const Nothing
+
+    render focused = renderWidget focused . getFocused
+
+treeWidget :: Widget parent -> [Widget value] -> Widget (parent, [value])
+treeWidget parent children
+  = Widget (True, parent, hlistWidget children) step render valueOf
+  where
+    step event focus = case progressInner event focus of
+      Just newFocus -> Just newFocus
+      Nothing       -> progressOuter event focus
+
+    progressInner event (True, parent, children)
+      = do newParent <- stepWidget event parent
+           return $ (True, newParent, children)
+    progressInner event (False, parent, children)
+      = do newChildren <- stepWidget event children
+           return $ (False, parent, newChildren)
+
+    progressOuter (KeyPress (Special ArrUp))   (False, p, c) = Just (True, p, c)
+    progressOuter (KeyPress (Special ArrDown)) (True, p, c)  = Just (False, p, c)
+    progressOuter _                            _             = Nothing
+
+    apply2List f [x,y] = f x y
+
+    render focused (True, parent, children)
+      = renderWidget focused parent `above` renderWidget False children
+    render focused (False, parent, children)
+      = renderWidget False parent `above` renderWidget focused children
+
+    valueOf (_, parent, children) = (valueOfWidget parent, valueOfWidget children)
+
+applicationWidget parent children
+  = mapWidgetValue (uncurry applyOperator) $ treeWidget parent children
+  where
+    applyOperator (Operator op) args = operate op args
+    applyOperator op            args = Left $ "invalid Operator: " ++ show op
+
+    operate Add [Num x, Num y] = Right $ Num (x + y)
+    operate Sub [Num x, Num y] = Right $ Num (x - y)
+    operate Mul [Num x, Num y] = Right $ Num (x * y)
+
+    operate _   args           = Left $ "invalid arguments: " ++ show args
+
+intWidget :: Int -> Widget Int
+intWidget def
+  = Widget def step render id
+  where step (KeyPress (Special ArrLeft))  x = Just (x - 1)
+        step (KeyPress (Special ArrRight)) x = Just (x + 1)
+        step _ _ = Nothing
+
+        render True  value = text ourTextStyle { textColor = orange } (show value)
+        render False value = text ourTextStyle (show value)
+        
+
+hlistWidget :: [Widget value] -> Widget [value]
+hlistWidget widgets
+  = Widget (focus widgets) step render valueOf
+  where
+    step event focus = case progressInner event focus of
+      Just newFocus -> Just newFocus
+      Nothing       -> progressOuter event focus
+
     progressInner event focus = do newWidget <- stepWidget event focusedWidget
                                    return $ setFocused newWidget focus
       where focusedWidget = getFocused focus
 
+    {-
     progressOuter :: GtkEvent
-                  -> (Bool, Focus Widget)
-                  -> (Bool, Focus Widget)
-    progressOuter (KeyPress (Letter 'i'))       (False, textInputs)
-      = (True, textInputs)
-    progressOuter (KeyPress (Special ArrLeft))  (insert, textInputs)
-      = (insert, maybeApply moveLeft textInputs)
-    progressOuter (KeyPress (Special ArrRight)) (insert, textInputs)
-      = (insert, maybeApply moveRight textInputs)
-    progressOuter (KeyPress (Special Escape))   (_,      textInputs)
-      = (False, textInputs)
-    progressOuter _ state = state
+                  -> Focus (Widget ...)
+                  -> Maybe (Focus (Widget ...))
+                  -}
+    progressOuter (KeyPress (Special ArrLeft))  = moveLeft
+    progressOuter (KeyPress (Special ArrRight)) = moveRight
+    --progressOuter (KeyPress (Letter 'a'))       = insertFocus (modeInputWidget "")
+    progressOuter (KeyPress (Letter 'x'))       = deleteFocus
+    progressOuter _                             = const Nothing
 
-    renderStuff :: (Bool, Focus Widget)
-                -> Form
-    renderStuff (insertMode, inputs)
-      = besides2 (renderShow insertMode)
-                 (besides $ focusAsList $ onFocused debugEnvelope $ mapFocus (pad . renderWidget) inputs)
-    pad = padded 15
+    render focused
+      = besides . focusAsList . mapFocusParts (renderWidget False) (renderWidget focused) (renderWidget False)
+
+    valueOf = map valueOfWidget . focusAsList
+
+widgetAsState :: Show value => Widget value -> State GtkEvent Form
+widgetAsState (Widget state step render valueOf)
+  = (maybeApply . step)
+    >>^ accum state ^>>
+    biapply above (render True) (centered . renderShow . valueOf)
+
+biapply merge f g x = merge (f x) (g x)
+
+
+type Tree a = (Bool,a,Focus a)
+
+treeFocus parent children = (True, parent, focus children)
+
+getTreeFocused (True, parent, _) = parent
+getTreeFocused (False, _, children) = getFocused children
+
+setTreeFocused x = onTreeFocused (const x)
+
+onTreeFocused f (True, parent, children) = (True, f parent, children)
+onTreeFocused f (False, parent, children) = (False, parent, onFocused f children)
+
+insertTree x (True, _, _) = Nothing
+insertTree x (False, parent, children) = do newChildren <- insertFocus x children
+                                            return (False, parent, newChildren)
+
+onChildren f (focus, parent, children) = (focus, parent, f children)
+
+moveTreeRight (True, _, _) = Nothing
+moveTreeRight (False, parent, children) = do newChildren <- moveRight children
+                                             return (False, parent, newChildren)
+
+moveTreeLeft (True, _, _) = Nothing
+moveTreeLeft (False, parent, children) = do newChildren <- moveLeft children
+                                            return (False, parent, newChildren)
+
+moveTreeUp (True, _, _) = Nothing
+moveTreeUp (False, parent, children) = Just (True, parent, children)
+
+moveTreeDown (False, _, _) = Nothing
+moveTreeDown (True, parent, children) = Just (False, parent, children)
+
+deleteTree (True, _, _) = Nothing
+deleteTree (False, parent, children) = do newChildren <- deleteFocus children
+                                          return (True, parent, newChildren)
 
 
 type Focus a = ([a],a,[a])
@@ -143,7 +304,35 @@ moveLeft (x:left, focus, right) = Just (left, x, focus:right)
 moveLeft ([], focus, right)     = Nothing
 
 onFocused f (left, focus, right) = (left, f focus, right)
+onFocusedLifted f (left, focus, right) = do newFocus <- f focus
+                                            return (left, newFocus, right)
 mapFocus f (left, focus, right) = (map f left, f focus, map f right)
 
+mapFocusParts leftF focusedF rightF (left, focus, right)
+  = (map leftF left, focusedF focus, map rightF right)
 
-main = runGtkZero modeTextInputWidgetTest
+insertFocus x (left, focus, right) = Just (focus:left, x, right)
+
+deleteFocus (left, focus, r:right) = Just (left, r, right)
+deleteFocus (l:left, focus, right) = Just (left, l, right)
+deleteFocus _                      = Nothing
+
+
+-- step function refactorisieren!!!!!
+
+main = runGtkZero $ widgetAsState $
+  applicationWidget
+    # operatorsSwitchWidget
+    # replicate 2 (modeWrapper $ intValueExprWidget 0)
+{-
+  treeWidget
+    # modeInputWidget "parent"
+    # replicate 5 (modeInputWidget "1")
+-}
+{-
+  treeWidget (modeInputWidget "Halloechen, Fadda") $
+    [treeWidget (modeInputWidget "Subfadda") $
+       [modeInputWidget "Pihmel", modeInputWidget "Votze"]
+    , modeWrapper $ modeInputWidget "Vagihna"
+    ]
+    -}
