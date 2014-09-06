5a1173f93f9f053248f65c44e2062f929f0a3898
Daniel Krueger <keenbug@gmail.com>
2014-09-06 21:13:26 +0200

    weg mit dem alten scheiss

--- FRPZeroGtk.hs
+++
@@ -1,9 +0,0 @@
-module FRPZeroGtk where
-
-import FRP
-import GtkUtils
-import FRPZero
-
-import Control.Arrow
-
-runGtkZero frpz = runGTK (foldEvents (\frpz event -> step event frpz) frpz >>> mapBehavior state)
