71788d8f049f0fcf3e49cc84801e717e6b21e513
Philipp Krüger <matheus.dev@gmail.com>
2014-09-18 19:40:29 +0200

    Minor changes. Changed .cabal file and project name

--- Demos/DemoSvg.hs
+++ Demos/DemoSvg.hs
@@ -4,10 +4,10 @@ import Demos.DemoUtilities
 import Graphics.Declarative.Form
 import Graphics.Declarative.Shape
 import Graphics.Declarative.Combinators
-import Graphics.Declarative.Util.FromSvg
+import Graphics.Declarative.Util.Svg
 
 main :: IO ()
 main = do
   source <- readFile "Demos/DemoSvg.hs"
-  svg <- formSvgFromFile "Ghostscript_Tiger.svg"
+  svg <- svgFormFile "Ghostscript_Tiger.svg"
   openGTK $ showSourceAndProduct source svg
